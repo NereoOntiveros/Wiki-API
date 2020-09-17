7317c7a96bd158235152ee94006c468446ef956f
Nereo Ontiveros <nerontiveros@gmail.com>
2020-09-17 20:55:18 +0100

    Patch a specific article

--- app.js
+++ app.js
@@ -83,6 +83,21 @@ app.route("/articles/:articleTitle")
       }
     }
   );
+})
+
+.patch(function(req, res){
+
+  Article.updateOne(
+    {title:req.params.articleTitle},
+    {$set:req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.")
+      }else{
+        res.send(err)
+      }
+    }
+  );
 });
 
 app.listen(3000,function(){
