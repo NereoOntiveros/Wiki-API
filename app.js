802856028eea8cd6debd513208ed49a4c2b4437e
Nereo Ontiveros <nerontiveros@gmail.com>
2020-09-17 17:38:30 +0100

    Get all articles

--- app.js
+++ app.js
@@ -10,13 +10,24 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true,useUnifiedTopology:true});
 
 const articleSchema = {
   title:String,
   content:String
 };
 const Article = mongoose.model("Article",articleSchema);
+
+app.get("/articles",function(req,res){
+  Article.find(function(err,foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+        res.send(err);
+    }
+
+  });
+});
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
